cb5551a45db72739aecbb5486ef0bf9e0b559272
an-Iceberg <a.g.marosi@bluewin.ch>
2022-04-22 17:43:01 +0200

    initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'phalcon/ide-stubs' => array(
+            'pretty_version' => 'v4.1.0',
+            'version' => '4.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phalcon/ide-stubs',
+            'aliases' => array(),
+            'reference' => '77384e4f42542f11b065b2a860b9bca2ea20ad5a',
+            'dev_requirement' => true,
+        ),
+    ),
+);
