68f1edbeabb548906609764a39088898c09f849f
unknown <a.g.marosi@bluewin.ch>
2022-06-16 17:29:13 +0200

    minor changes

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,4 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Phalcon\\Migrations\\' => array($vendorDir . '/phalcon/migrations/src'),
+    'Phalcon\\Cop\\' => array($vendorDir . '/phalcon/cli-options-parser/src'),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -6,7 +6,7 @@
     'aliases' => 
     array (
     ),
-    'reference' => 'f19a11d5bbad5f9fe6b7801b9069e1e083cd1cb1',
+    'reference' => '34ec31049586f1e09bcc6a770f3e83a625e08999',
     'name' => '__root__',
   ),
   'versions' => 
@@ -18,7 +18,34 @@
       'aliases' => 
       array (
       ),
-      'reference' => 'f19a11d5bbad5f9fe6b7801b9069e1e083cd1cb1',
+      'reference' => '34ec31049586f1e09bcc6a770f3e83a625e08999',
+    ),
+    'nette/php-generator' => 
+    array (
+      'pretty_version' => 'v3.6.7',
+      'version' => '3.6.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9ba414c9895fd9420887f20eeb4eabde123677f',
+    ),
+    'nette/utils' => 
+    array (
+      'pretty_version' => 'v3.2.7',
+      'version' => '3.2.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0af4e3de4df9f1543534beab255ccf459e7a2c99',
+    ),
+    'phalcon/cli-options-parser' => 
+    array (
+      'pretty_version' => 'v1.3.0',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1c5a7d0db23a88d8ba14646af75464cbbd115251',
     ),
     'phalcon/ide-stubs' => 
     array (
@@ -28,6 +55,15 @@
       array (
       ),
       'reference' => '77384e4f42542f11b065b2a860b9bca2ea20ad5a',
+    ),
+    'phalcon/migrations' => 
+    array (
+      'pretty_version' => 'v2.2.4',
+      'version' => '2.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df71fb394a9a0840e27ff79d25ac7276dd7f9217',
     ),
   ),
 );
