096be7fd512693cd74d4c7b2b9a66d02e53d26c0
Priscilla | Liala | Sandra <a.g.marosi@bluewin.ch>
2025-01-07 21:19:27 +0100

    Added boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod routes;
 
 use actix_files::Files;
 use actix_web::{App, HttpServer};
-use routes::{angebot, buchung, gutschein, index, kontakt, standort, ueber_mich};
+use routes::{create_blog, index};
 use tera::Tera;
 use lazy_static::lazy_static;
 
@@ -32,12 +32,7 @@ async fn main() -> std::io::Result<()>
   {
     App::new()
       .service(index)
-      .service(buchung)
-      .service(ueber_mich)
-      .service(standort)
-      .service(angebot)
-      .service(gutschein)
-      .service(kontakt)
+      .service(create_blog)
       .service(Files::new("/", "./static"))
   })
   .bind(("127.0.0.1", 8080))?
--- src/routes.rs
+++ src/routes.rs
@@ -6,82 +6,22 @@ use crate::TERA;
 async fn index() -> HttpResponse
 {
   let mut context = Context::new();
-  context.insert("page_title", "SzM");
-  context.insert("header_text", "SzM");
+  context.insert("page_title", "Homepage");
+  context.insert("header_text", "Header");
 
   let body = TERA.render("pages/index.html", &context).expect("Failed to render index.html");
 
   HttpResponse::Ok().body(body)
 }
 
-#[get("/buchung")]
-async fn buchung() -> HttpResponse
+#[get("/create_blog")]
+async fn create_blog() -> HttpResponse
 {
   let mut context = Context::new();
-  context.insert("page_title", "SzM — Buchung");
-  context.insert("header_text", "Buchung erstellen");
+  context.insert("page_title", "Create blog");
+  // context.insert("header_text", "Create blog");
 
-  let body = TERA.render("pages/buchung.html", &context).expect("Failed to render buchung.html");
-
-  HttpResponse::Ok().body(body)
-}
-
-#[get("/ueber-mich")]
-async fn ueber_mich() -> HttpResponse
-{
-  let mut context = Context::new();
-  context.insert("page_title", "SzM — Über mich");
-  context.insert("header_text", "Über mich");
-
-  let body = TERA.render("pages/ueber-mich.html", &context).expect("Failed to render ueber-mich.html");
-
-  HttpResponse::Ok().body(body)
-}
-
-#[get("/standort")]
-async fn standort() -> HttpResponse
-{
-  let mut context = Context::new();
-  context.insert("page_title", "SzM — Standort");
-  context.insert("header_text", "Standort");
-
-  let body = TERA.render("pages/standort.html", &context).expect("Failed to render standort.html");
-
-  HttpResponse::Ok().body(body)
-}
-
-#[get("/angebot")]
-async fn angebot() -> HttpResponse
-{
-  let mut context = Context::new();
-  context.insert("page_title", "SzM — Angebot");
-  context.insert("header_text", "Angebot");
-
-  let body = TERA.render("pages/angebot.html", &context).expect("Failed to render angebot.html");
-
-  HttpResponse::Ok().body(body)
-}
-
-#[get("/gutschein")]
-async fn gutschein() -> HttpResponse
-{
-  let mut context = Context::new();
-  context.insert("page_title", "SzM — Gutschein");
-  context.insert("header_text", "Gutschein");
-
-  let body = TERA.render("pages/gutschein.html", &context).expect("Failed to render gutschein.html");
-
-  HttpResponse::Ok().body(body)
-}
-
-#[get("/kontakt")]
-async fn kontakt() -> HttpResponse
-{
-  let mut context = Context::new();
-  context.insert("page_title", "SzM — Kontakt");
-  context.insert("header_text", "Kontakt");
-
-  let body = TERA.render("pages/kontakt.html", &context).expect("Failed to render kontakt.html");
+  let body = TERA.render("pages/create_blog.html", &context).expect("Failed to render create_blog.html");
 
   HttpResponse::Ok().body(body)
 }
